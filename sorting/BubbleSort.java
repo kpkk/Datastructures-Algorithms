13e2d06a2296ee28ae3a061e2f658d3aed9b02d6
Krishna Pradeep kumar Kadarla <49063654+kpkk@users.noreply.github.com>
2024-12-09 07:21:18 +0530

    Create BubbleSort.java

---
+++ sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class BubbleSort {
+
+    public static int[] bubbleSort(int[] nums){
+        boolean swapped=false;
+        for(int i=0;i<nums.length;i++){
+            swapped=false;
+            for(int j=0;j<nums.length-1;j++){
+                // if the left ele is greater than right ele
+                if(nums[j]>nums[j+1]){
+                    // swap
+                    int temp= nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped) break;
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums={5,3,1,4,2};
+        int[] output= bubbleSort(nums);
+    }
+}
